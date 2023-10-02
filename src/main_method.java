4590e5b05d38c5ea604384000d8a32365628cdea
hadihage <145401012+hadihage@users.noreply.github.com>
2023-10-02 19:02:04 -0400

    test

--- src/main_method.java
+++ src/main_method.java
@@ -4,7 +4,7 @@ public class main_method {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, World!");
-		System.out.println("alternate more github playing");
+		System.out.println("alttrnate more github playing");
 		
 		
 	}
