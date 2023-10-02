c120f9e8a20dd26e25722d362d023327061c7ca4
hadihage <145401012+hadihage@users.noreply.github.com>
2023-10-02 19:24:42 -0400

    test

--- src/main_method.java
+++ src/main_method.java
@@ -4,7 +4,7 @@ public class main_method {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, World!");
-		System.out.println("changin it for a third time more github playing");
+		System.out.println("changin it for a forth time more github playing");
 		
 		
 	}
