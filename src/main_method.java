af45f403b3e578f451422c1c5854bc05db4f2329
hadihage <145401012+hadihage@users.noreply.github.com>
2023-10-02 18:56:03 -0400

    pushing alternate place

--- src/main_method.java
+++ src/main_method.java
@@ -4,9 +4,10 @@ public class main_method {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, World!");
-		System.out.println("more github playing");
+		System.out.println("alternate more github playing");
 		
 		
 	}
 
 }
+     
